46a3d806372087504bb91c2c1e2ffab8497a8c7a
🤔ohju <ohju.lim@gmail.com>
2019-02-09 17:33:10 +0900

    V2
    계산하기 버튼을 누르지 않아도 입력한 즉시 계산 결과 확인 가능.

--- TicketCountV1/ViewController.swift
+++ TicketCountV1/ViewController.swift
@@ -17,50 +17,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
 	
 	let ticketPrice = 4500 //식권 가격
 	@IBOutlet weak var inputField: UITextField! //사용자 입력 값
+	@IBOutlet weak var ticketCountLabel: UILabel!
+	@IBOutlet weak var changeLabel: UILabel!
 	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		inputField.becomeFirstResponder()
+		initializeLabels()
 	}
 	
-	@IBAction func showAlert() {
-		let orderValue:Int? = Int(inputField.text!) // 텍스트필드에 입력 값을 Int형으로 변환하기
-		let ticketCount:Int // 변환된 Int 값을 갖고 아래에서 계산
-		if orderValue! % ticketPrice == 0 { // 나눗셈이 딱 떨어지면(주문가격이 식권가격의 배수일 경우)
-			ticketCount = orderValue! / ticketPrice // 나눗셈의 몫이 곧 필요한 식권 수 이고,
-		} else { // 나눗셈이 딱 떨어지지않으면 나눗셈의 몫에 1을 더한 값이 필요한 최소 식권 수.
-			ticketCount = orderValue! / ticketPrice + 1
-		}
-		let change = (ticketPrice * ticketCount) - orderValue!
-		
-		let title:String
-		let message:String
-		
-		if orderValue == ticketPrice {
-			title = "식권값 몰라요?"
-			message = "찌릿🤨"
-		} else if orderValue! % ticketPrice == 0 {
-			title = "\(ticketCount)장 내세요"
-			message = "거스름돈은 없어요"
-		} else {
-			title = "\(ticketCount)장 내시고"
-			message = "\(change)원 받으세요🤑"
-		}
-
-		let alert = UIAlertController(title: title,
-																	message: message,
-																	preferredStyle: .alert)
-		
-		let action = UIAlertAction(title: "ㅇㅋ", style: .default,
-															 handler: nil)
-		
-		alert.addAction(action)
-		
-		present(alert, animated: true, completion: nil)
+	func initializeLabels() {
+		ticketCountLabel.text = "🎟 식권"
+		changeLabel.text = "🤑 거스름돈"
 	}
-	
-	@IBOutlet weak var showAlertButton: UIButton!
 	
 	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 		
@@ -68,12 +38,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
 		let stringRange = Range(range, in:oldText)!
 		let newText = oldText.replacingCharacters(in: stringRange,
 																							with: string)
-		showAlertButton.isEnabled = !newText.isEmpty
+		let ticketCount:Int
+		if let orderValue = Int(newText) {
+			if orderValue % ticketPrice == 0 { // 나눗셈이 딱 떨어지면(주문가격이 식권가격의 배수일 경우)
+				ticketCount = orderValue / ticketPrice // 나눗셈의 몫이 곧 필요한 식권 수 이고,
+			} else { // 나눗셈이 딱 떨어지지않으면 나눗셈의 몫에 1을 더한 값이 필요한 최소 식권 수.
+				ticketCount = orderValue / ticketPrice + 1
+			}
+			let change = (ticketPrice * ticketCount) - orderValue
+			
+			if orderValue == ticketPrice {
+				ticketCountLabel.text = "🎟 \(ticketCount)장 내세요"
+				changeLabel.text = "👌 거스름돈 없어요"
+			} else if orderValue % ticketPrice == 0 {
+				ticketCountLabel.text = "🎟 \(ticketCount)장 내세요"
+				changeLabel.text = "👌 거스름돈 없어요"
+			} else {
+				ticketCountLabel.text = "🎟 \(ticketCount)장 내고"
+				changeLabel.text = "🤑 \(change)원 받으세요"
+			}
+		} else {
+			initializeLabels()
+		}
 		return true
 	}
 	
 	func textFieldShouldClear(_ textField: UITextField) -> Bool {
-		showAlertButton.isEnabled = false
+		initializeLabels()
 		return true
 	}
 
